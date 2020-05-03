1ab81169160d47168b01c5b3d410ef22ac95ee8f
James Johnson <jtaylorjohnson@gmail.com>
2020-05-03 19:56:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,8 +45,12 @@ attr_reader :id
     student
   end
   
-  def self.new_from_db
-    
+  def self.new_from_db(row)
+  student = self.new
+  student.id = row[0]
+  student.name =  row[1]
+  student.length = row[2]
+  student
   end
   
   def self.find_by_name(name)
