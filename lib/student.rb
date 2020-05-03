30104c3bd611e9470176152f6cbcde51f764df2b
James Johnson <jtaylorjohnson@gmail.com>
2020-05-03 20:23:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,9 +50,9 @@ attr_reader :id
   end
   
   def self.new_from_db(row)
-  row[0] = id
-  row[1] = name
-  row[2] = grade
+  id = row[0]
+  name = row[1]
+  grade = row[2]
   student = self.new(id, name, grade)
   #student.id = row[0]
   #student.name =  row[1]
