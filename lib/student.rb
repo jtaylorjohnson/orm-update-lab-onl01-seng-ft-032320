b44f569e7cd5c39b3dc383407af01772cc1f6085
James Johnson <jtaylorjohnson@gmail.com>
2020-05-03 19:50:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,23 +25,34 @@ attr_reader :id
   
   def self.drop_table
     sql = <<-SQL
-      
+      DROP TABLE students
+      SQL
+    DB[:conn].execute(sql)
   end
   
   def save
-    
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+      SQL
+    DB[:conn].execute(sql, self.name, self.grade) 
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
   end
   
-  def self.create 
-    
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
   end
   
   def self.new_from_db
     
   end
   
-  def self.find_by_name
-    
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DC[:conn].execute(sql, name)[0]
+    Student.new(result[0], result[1], result[2])
   end
   
   def update
