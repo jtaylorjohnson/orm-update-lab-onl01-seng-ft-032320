81708c7e1fc91852aa4a2cf91b8c6412aff00600
James Johnson <jtaylorjohnson@gmail.com>
2020-05-03 19:40:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,16 +5,27 @@ class Student
 attr_accessor :name, :grade
 attr_reader :id 
 
-  def initialize
-    
+  def initialize(id=nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
   end
   
   def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+      ) 
+        SQL
     
+    DB[:conn].execute(sql)
   end
   
   def self.drop_table
-    
+    sql = <<-SQL
+      
   end
   
   def save
