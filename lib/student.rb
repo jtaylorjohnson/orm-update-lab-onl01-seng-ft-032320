7673d8bd940b46aed3852019f08aaa2b31724063
James Johnson <jtaylorjohnson@gmail.com>
2020-05-03 20:23:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,9 +54,9 @@ attr_reader :id
   row[1] = name
   row[2] = grade
   student = self.new(id, name, grade)
-  student.id = row[0]
-  student.name =  row[1]
-  student.grade = row[2]
+  #student.id = row[0]
+  #student.name =  row[1]
+  #student.grade = row[2]
   student
   end
   
